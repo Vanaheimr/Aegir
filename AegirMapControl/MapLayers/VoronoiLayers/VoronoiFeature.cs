fc34fb2e67ffd0034133a4edac2fe6f903cb327d
Achim Friedland <github@ahzf.de>
2013-09-20 20:51:48 +0200

    Started to implement a voronoi layer

---
+++ AegirMapControl/MapLayers/VoronoiLayers/VoronoiFeature.cs
@@ -0,0 +1,207 @@
+﻿/*
+ * Copyright (c) 2011-2013 Achim 'ahzf' Friedland <achim@graph-database.org>
+ * This file is part of Aegir <http://www.github.com/Vanaheimr/Aegir>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * You may obtain a copy of the License at
+ *   http://www.gnu.org/licenses/gpl.html
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ */
+
+#region Usings
+
+using System;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+#endregion
+
+namespace eu.Vanaheimr.Aegir
+{
+
+    /// <summary>
+    /// A feature on a feature layer of an Aegir map.
+    /// </summary>
+    public class VoronoiFeature : Shape,
+                                  IEquatable<VoronoiFeature>, IComparable<VoronoiFeature>,
+                                  IEquatable<String>,         IComparable<String>
+    {
+
+        #region Properties
+
+        #region Id
+
+        /// <summary>
+        /// The unique identification of this map feature.
+        /// </summary>
+        public String Id { get; private set; }
+
+        #endregion
+
+        #region Latitude/Longitude/Altitude
+
+        public GeoCoordinate GeoCoordinate { get; private set; }
+
+        /// <summary>
+        /// The latitude of the feature.
+        /// </summary>
+        public Latitude  Latitude   { get { return this.GeoCoordinate.Latitude; } }
+
+        /// <summary>
+        /// The longitude of the feature.
+        /// </summary>
+        public Longitude Longitude  { get { return this.GeoCoordinate.Longitude; } }
+
+        /// <summary>
+        /// The altitude to the feature.
+        /// </summary>
+        public Altitude  Altitude   { get { return this.GeoCoordinate.Altitude; } }
+
+        #endregion
+
+        #region Geometry
+
+        private readonly Geometry Geometry;
+
+        /// <summary>
+        /// The geometry of the feature.
+        /// </summary>
+        protected override Geometry DefiningGeometry
+        {
+            get
+            {
+                return Geometry;
+            }
+        }
+
+        #endregion
+
+        #endregion
+
+        #region Constructor(s)
+
+        #region Feature(Id, GeoCoordinate)
+
+        /// <summary>
+        /// Create a new map feature.
+        /// </summary>
+        /// <param name="Id">The unique identification of this map feature.</param>
+        /// <param name="GeoCoordinate">The geo coordinate of the feature.</param>
+        public VoronoiFeature(String         Id,
+                              GeoCoordinate  GeoCoordinate)
+        {
+            this.Id             = Id;
+            this.GeoCoordinate  = GeoCoordinate;
+        }
+
+        #endregion
+
+        #endregion
+
+
+        #region IComparable<Identifier/Feature> Members
+
+        #region CompareTo(Identifier)
+
+        /// <summary>
+        /// Compares two instances of this object.
+        /// </summary>
+        /// <param name="Identifier">An object to compare with.</param>
+        public Int32 CompareTo(String Identifier)
+        {
+
+            if ((Object) Identifier == null)
+                throw new ArgumentNullException("The given feature identifier must not be null!");
+
+            return this.Id.CompareTo(Identifier);
+
+        }
+
+        #endregion
+
+        #region CompareTo(Feature)
+
+        /// <summary>
+        /// Compares two instances of this object.
+        /// </summary>
+        /// <param name="Identifier">An object to compare with.</param>
+        public Int32 CompareTo(VoronoiFeature Feature)
+        {
+
+            if ((Object) Feature == null)
+                throw new ArgumentNullException("The given feature must not be null!");
+
+            return this.Id.CompareTo(Feature.Id);
+
+        }
+
+        #endregion
+
+        #endregion
+
+        #region IEquatable<Identifier/Feature> Members
+
+        #region Equals(Identifier)
+
+        /// <summary>
+        /// Compares two feature identifiers for equality.
+        /// </summary>
+        /// <param name="Identifier">An identifier to compare with.</param>
+        /// <returns>True if both match; False otherwise.</returns>
+        public Boolean Equals(String Identifier)
+        {
+
+            if ((Object) Identifier == null || Identifier == "")
+                return false;
+
+            return this.Id.Equals(Identifier);
+
+        }
+
+        #endregion
+
+        #region Equals(Feature)
+
+        /// <summary>
+        /// Compares two features for equality.
+        /// </summary>
+        /// <param name="FeatureLayer">A feature to compare with.</param>
+        /// <returns>True if both match; False otherwise.</returns>
+        public Boolean Equals(VoronoiFeature Feature)
+        {
+
+            if ((Object) Feature == null)
+                return false;
+
+            return this.Id.Equals(Feature.Id);
+
+        }
+
+        #endregion
+
+        #endregion
+
+        #region GetHashCode()
+
+        /// <summary>
+        /// Returns the hash code for this feature layer.
+        /// </summary>
+        public new Int32 GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+
+        #endregion
+
+    }
+
+}
+
---
+++ AegirMapControl/MapLayers/VoronoiLayers/VoronoiLayer.cs
@@ -0,0 +1,264 @@
+﻿/*
+ * Copyright (c) 2011-2013 Achim 'ahzf' Friedland <achim@graph-database.org>
+ * This file is part of Aegir <http://www.github.com/Vanaheimr/Aegir>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * You may obtain a copy of the License at
+ *   http://www.gnu.org/licenses/gpl.html
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ */
+
+#region Usings
+
+using System;
+using System.Windows.Media;
+using System.ComponentModel;
+using System.Windows.Controls;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using eu.Vanaheimr.Illias.Geometry;
+using eu.Vanaheimr.Aegir.Controls;
+
+#endregion
+
+namespace eu.Vanaheimr.Aegir
+{
+
+    /// <summary>
+    /// A feature layer for visualizing map features.
+    /// </summary>
+    public class VoronoiLayer : AMapLayer
+    {
+
+        #region Data
+
+        private readonly List<VoronoiFeature> VoronoiFeatures;
+        private List<ITriangle<Double>> TriangleList;
+
+        #endregion
+
+        #region Properties
+
+        #region Delaunay
+        #endregion
+
+        #endregion
+
+        #region Constructor(s)
+
+        #region VoronoiLayer(Id, MapControl, ZIndex)
+
+        /// <summary>
+        /// Creates a new feature layer for visualizing map features.
+        /// </summary>
+        /// <param name="Name">The identification string of this feature layer.</param>
+        /// <param name="MapControl">The parent map control.</param>
+        /// <param name="ZIndex">The z-index of this feature layer.</param>
+        public VoronoiLayer(String Id, MapControl MapControl, Int32 ZIndex)
+            : base(Id, MapControl, ZIndex)
+        {
+            this.VoronoiFeatures = new List<VoronoiFeature>();
+        }
+
+        #endregion
+
+        #endregion
+
+
+        #region AddFeature(Id, Latitude, Longitude, Fill, Stroke, StrokeThickness)
+
+        /// <summary>
+        /// Add a map feature of the given dimensions at the given geo coordinate.
+        /// </summary>
+        /// <param name="Id">The unique identification of the feature.</param>
+        /// <param name="Latitude">The latitude of the feature.</param>
+        /// <param name="Longitude">The longitude of the feature.</param>
+        /// <param name="Fill">The fill brush of the feature.</param>
+        /// <param name="Stroke">The stroke brush of the feature.</param>
+        /// <param name="StrokeThickness">The thickness of the stroke.</param>
+        public Feature AddFeature(String     Id,
+                                  Latitude   Latitude,
+                                  Longitude  Longitude,
+                                  Brush      Fill,
+                                  Brush      Stroke,
+                                  Double     StrokeThickness)
+
+        {
+
+            return AddFeature(Id,
+                              new GeoCoordinate(Latitude, Longitude),
+                              Fill,
+                              Stroke, StrokeThickness);
+
+        }
+
+        #endregion
+
+        #region AddFeature(Id, Latitude, Longitude, Altitude, Fill, Stroke, StrokeThickness)
+
+        /// <summary>
+        /// Add a map feature of the given dimensions at the given geo coordinate.
+        /// </summary>
+        /// <param name="Id">The unique identification of the feature.</param>
+        /// <param name="Latitude">The latitude of the feature.</param>
+        /// <param name="Longitude">The longitude of the feature.</param>
+        /// <param name="Altitude">The altitude of the feature.</param>
+        /// <param name="Fill">The fill brush of the feature.</param>
+        /// <param name="Stroke">The stroke brush of the feature.</param>
+        /// <param name="StrokeThickness">The thickness of the stroke.</param>
+        public Feature AddFeature(String Id,
+                                  Latitude   Latitude,
+                                  Longitude  Longitude,
+                                  Altitude   Altitude,
+                                  Brush      Fill,
+                                  Brush      Stroke,
+                                  Double     StrokeThickness)
+
+        {
+
+            return AddFeature(Id,
+                              new GeoCoordinate(Latitude, Longitude, Altitude),
+                              Fill,
+                              Stroke, StrokeThickness);
+
+        }
+
+        #endregion
+
+        #region AddFeature(Id, GeoCoordinate, Width, Height, Fill, Stroke, StrokeThickness)
+
+        /// <summary>
+        /// Add a map feature of the given dimensions at the given geo coordinate.
+        /// </summary>
+        /// <param name="Id">The unique identification of the feature.</param>
+        /// <param name="GeoCoordinate">The geo coordinate of the feature.</param>
+        /// <param name="Fill">The fill brush of the feature.</param>
+        /// <param name="Stroke">The stroke brush of the feature.</param>
+        /// <param name="StrokeThickness">The thickness of the stroke.</param>
+        public VoronoiFeature AddFeature(String         Id,
+                                         GeoCoordinate  GeoCoordinate,
+                                         Brush          Fill,
+                                         Brush          Stroke,
+                                         Double         StrokeThickness)
+        {
+
+            var Feature = new VoronoiFeature(Id, GeoCoordinate) {
+                Fill             = Fill,
+                Stroke           = Stroke,
+                StrokeThickness  = StrokeThickness
+            };
+
+            this.VoronoiFeatures.Add(Feature);
+            Calc();
+
+            // The position on the map will be set within the PaintMap() method!
+            this.Children.Add(Feature);
+
+            return Feature;
+
+        }
+
+        #endregion
+
+
+        private void Calc()
+        {
+
+            if (VoronoiFeatures.Count > 2)
+            {
+
+                #region Draw Delaunay
+
+                //if (DelaunayCheckBox.IsChecked == true)
+                //{
+
+                    //Task.Factory.StartNew(() =>
+                    //{
+
+                        if (TriangleList == null)
+                            TriangleList = VoronoiFeatures.DelaunayTriangulation().ToList();
+
+                        foreach (var _Triangle in TriangleList)
+                        {
+                            DrawLine(_Triangle.P1.X, _Triangle.P1.Y, _Triangle.P2.X, _Triangle.P2.Y, Colors.Blue);
+                            DrawLine(_Triangle.P2.X, _Triangle.P2.Y, _Triangle.P3.X, _Triangle.P3.Y, Colors.Blue);
+                            DrawLine(_Triangle.P3.X, _Triangle.P3.Y, _Triangle.P1.X, _Triangle.P1.Y, Colors.Blue);
+                        }
+
+                    //}, _CancellationToken, TaskCreationOptions.None, _TaskSchedulerContext);
+
+                //}
+
+                #endregion
+
+
+            }
+
+        }
+
+
+        #region (override) Move(X, Y)
+
+        /// <summary>
+        /// Move all the shapes on this mapping layer.
+        /// </summary>
+        /// <param name="X">X</param>
+        /// <param name="Y">Y</param>
+        public override void Move(Double X, Double Y)
+        {
+            Redraw();
+        }
+
+        #endregion
+
+        #region (override) Redraw()
+
+        /// <summary>
+        /// Redraws this mapping layer.
+        /// </summary>
+        public override void Redraw()
+        {
+
+            if (this.IsVisible && !IsCurrentlyPainting)
+            {
+
+                IsCurrentlyPainting = true;
+
+                if (!DesignerProperties.GetIsInDesignMode(this))
+                {
+
+                    this.Children.
+                         ForEach<Feature>(AFeature => {
+
+                             var ScreenXY = GeoCalculations.GeoCoordinate2ScreenXY(AFeature.Latitude,
+                                                                                   AFeature.Longitude,
+                                                                                   MapControl.ZoomLevel);
+
+                             Canvas.SetLeft(AFeature, this.MapControl.ScreenOffset.X + ScreenXY.X);
+                             Canvas.SetTop (AFeature, this.MapControl.ScreenOffset.Y + ScreenXY.Y);
+
+                         });
+
+                }
+
+                IsCurrentlyPainting = false;
+
+            }
+
+        }
+
+        #endregion
+
+
+    }
+
+}
